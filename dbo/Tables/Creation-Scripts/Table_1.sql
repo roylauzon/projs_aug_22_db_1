5dff848e97571f496638ee973c687542db5ddfcb
roylauzon <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>
2021-08-22 13:27:24 +0000

    added 2 first tables

---
+++ dbo/Tables/Creation-Scripts/Table_1.sql
@@ -0,0 +1,1 @@
+CREATE TABLE [dbo].[Table_1](	  [id1] NCHAR(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL)
---
+++ dbo/Tables/Creation-Scripts/Table_2.sql
@@ -0,0 +1,1 @@
+CREATE TABLE [dbo].[Table_2](	  [id2] NCHAR(10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL)
